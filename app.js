349705f388bc4f1bc033b410427410177abebb9b
Your Name <jcso2@aluno.ifal.edu.br>
2022-12-18 14:41:06 -0300

    mudanças

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ require('dotenv').config()
 //Conexão com o mongo
   const mongoose= require("mongoose")
 
-  const DB_USER = process.env.DB_USER
-  const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD) 
+  /*const DB_USER = process.env.DB_USER
+  const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD) */
 
-  const url = `mongodb://${DB_USER}:${DB_PASSWORD}@ac-djbanuf-shard-00-00.qhiu5ry.mongodb.net:27017,ac-djbanuf-shard-00-01.qhiu5ry.mongodb.net:27017,ac-djbanuf-shard-00-02.qhiu5ry.mongodb.net:27017/Api-MemStudy?ssl=true&replicaSet=atlas-e2e8c0-shard-0&authSource=admin&retryWrites=true&w=majority`
+  const url = "mongodb://julio:96545146@ac-djbanuf-shard-00-00.qhiu5ry.mongodb.net:27017,ac-djbanuf-shard-00-01.qhiu5ry.mongodb.net:27017,ac-djbanuf-shard-00-02.qhiu5ry.mongodb.net:27017/Api-MemStudy?ssl=true&replicaSet=atlas-e2e8c0-shard-0&authSource=admin&retryWrites=true&w=majority"
   mongoose.connect(url, {
     }).then(() => {
       console.log("MongoDB Conectado!!!");
@@ -24,5 +24,5 @@ const PORT = process.env.PORT || 3000;
 
 
 app.listen(PORT, () => {
-  console.log("Aplicação rodando na porta 3000!");
+  console.log("Aplicação rodando na porta: "+PORT);
 })
